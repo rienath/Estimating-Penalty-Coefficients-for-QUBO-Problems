368b1ee86afb903673089e019afa59601700f406
rienath <infernalion@hotmail.com>
2022-05-05 19:43:19 +0100

    Add all methods from Statistical Significance to the package

--- Project and Deliverables/Implementation/code/table.py
+++ Project and Deliverables/Implementation/code/table.py
@@ -3,6 +3,8 @@ from itertools import chain, cycle
 import pandas as pd
 
 
+from scipy import stats
+
 class Table:
     @staticmethod
     def display_side_by_side(*args, titles=cycle([''])):
@@ -80,3 +82,79 @@ class Table:
         stats.loc['SD'] = sd
 
         return stats
+
+    '''
+    Compares if the mean number of broken constraints is significantly different across all
+    instances of the same problem
+    '''
+    def significance_test(first_greedy, first_sa, first_tabu, second_greedy, second_sa, second_tabu):
+        # Flatten the dataframes
+        flat = lambda df: df.to_numpy().flatten()
+        a1, a2, a3 = flat(first_greedy), flat(first_sa), flat(first_tabu)
+        b1, b2, b3 = flat(second_greedy), flat(second_sa), flat(second_tabu)
+        # Calculate statistics
+        significance = pd.DataFrame(index=['t-statistic','p-value'])
+        significance['Greedy Algorithm'] = stats.ttest_ind(a1, b1)
+        significance['Simulated Annealing'] = stats.ttest_ind(a2, b2)
+        significance['Tabu Search'] = stats.ttest_ind(a3, b3)
+        return significance
+
+    '''
+    A detailed version of the above significance test, where all the problem instances are compared
+    independantly.
+    '''
+    def detailed_significance_test(first_greedy, first_sa, first_tabu, second_greedy, second_sa, second_tabu):
+        column_names = ['Greedy Algorithm t-statistic', 'Greedy Algorithm p-value', 
+                        'Simulated Annealing t-statistic', 'Simulated Annealing p-value',
+                        'Tabu Search t-statistic', 'Tabu Search p-value']
+        detailed_significance = pd.DataFrame(columns=column_names)
+        detailed_significance.index.name = 'Problem'
+        for i in range(len(first_greedy)):
+            # Flatten the rows
+            flat = lambda column: column.iloc[i].values
+            a1, a2, a3 = flat(first_greedy), flat(first_sa), flat(first_tabu)
+            b1, b2, b3 = flat(second_greedy), flat(second_sa), flat(second_tabu)
+            # Calculate statistica
+            greedy = stats.ttest_ind(a1, b1)
+            sa = stats.ttest_ind(a2, b2)
+            tabu = stats.ttest_ind(a3, b3)
+            # Populate table
+            new = [greedy[0], greedy[1], sa[0], sa[1], tabu[0], tabu[1]]
+            detailed_significance.loc[f'{i}'] = new
+        return detailed_significance
+
+    '''
+    Acccepts detailed_significance_test method output.
+    Returns the number of problems, where VL breaks significantly less constraints (0),
+    there is no significant difference (1), where VL breaks significantly more constraints (2).
+    It is impossible for VL to break more constraints as it has the largest M, but this number
+    is included for testing purposes.
+    '''
+    def find_best_significance(sig_data):
+        vl_better = [0, 0, 0]
+        no_sig_diff = [0, 0, 0] 
+        vl_worse =  [0, 0, 0]
+        
+        # Use column name based on the algorithm we are considering
+        t = ['Greedy Algorithm t-statistic',
+            'Simulated Annealing t-statistic',
+            'Tabu Search t-statistic']
+        p = ['Greedy Algorithm p-value',
+             'Simulated Annealing p-value',
+             'Tabu Search p-value']
+        
+        # Count
+        for i in range(3):
+            for _, row in sig_data.iterrows():
+                if row[t[i]] > 0 and row[p[i]] <= 0.05:
+                    vl_worse[i] += 1
+                elif row[t[i]] < 0 and row[p[i]] <= 0.05:
+                    vl_better[i] += 1
+                else:
+                    no_sig_diff[i] += 1
+        
+        output = pd.DataFrame({'VL was better': vl_better,
+                               'No significant difference': no_sig_diff,
+                               'VL was worse': vl_worse}, index=['Greedy', 'SA', 'TS'])
+        
+        return output
